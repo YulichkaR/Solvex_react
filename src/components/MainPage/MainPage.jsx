fe74353624f202c85e1b8be85dab9b81b4300759
YulichkaR <yuliaromanyk2551@gmail.com>
2023-03-05 18:36:03 +0200

    created hookahs and pubs cards

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -3,12 +3,16 @@ import { Header } from "../common/Header/Header";
 import { Footer } from "../common/Header/Footer";
 import { BestLvivPlaces } from "./BestLvivPlaces/BestLvivPlaces";
 import { RestaurantCards } from "./Cards/Card/RestaurantCards";
+import { HookahsCards } from "./Cards/Card/HookahsCard";
+import { PubsCards } from "./Cards/Card/PubsCard";
 export const MainPage = () => {
   return (
     <div className="main-page">
       <Header />
       <BestLvivPlaces />
       <RestaurantCards />
+      <HookahsCards />
+      <PubsCards />
       <Footer />
     </div>
   );
