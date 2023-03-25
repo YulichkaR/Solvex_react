f72e00609b55444ccb8fe6417485a4ddd854364c
YuliaRomanyk <117859473+YulichkaR@users.noreply.github.com>
2023-03-25 14:19:12 +0200

    Revert "created hookahs and pubs cards"

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Header } from "../common/Header/Header";
+<<<<<<< HEAD
 import { BestLvivPlaces } from "./BestLvivPlaces/BestLvivPlaces";
 import { Footer } from "../common/Header/Footer";
+=======
+import { Footer } from "../common/Header/Footer";
+import { BestLvivPlaces } from "./BestLvivPlaces/BestLvivPlaces";
 import { RestaurantCards } from "./Cards/Card/RestaurantCards";
-import { HookahsCards } from "./Cards/Card/HookahsCard";
-import { PubsCards } from "./Cards/Card/PubsCard";
-
+>>>>>>> 95ad8f07b1a16012c9e70376dad563563e9333b7
 export const MainPage = () => {
   return (
     <div className="main-page">
       <Header />
       <BestLvivPlaces />
+<<<<<<< HEAD
+=======
       <RestaurantCards />
-      <HookahsCards />
-      <PubsCards />
+>>>>>>> 95ad8f07b1a16012c9e70376dad563563e9333b7
       <Footer />
     </div>
   );
